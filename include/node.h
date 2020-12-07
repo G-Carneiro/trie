c3953b8b22a21885259a27ab3aac8edcff028c5d
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-12-07 17:23:04 -0300

    inicio da implementação de node

--- include/node.h
+++ include/node.h
@@ -1,4 +1,33 @@
 #ifndef INCLUDE_NODE_H
 #define INCLUDE_NODE_H
 
-#endif
+#include <cstdint>
+
+namespace structures {
+//! Nodo
+/*
+   Nodo da estrutura trie.
+*/
+class Node {
+ public:
+  //! Construtor
+  /*
+     Construtor padrão da estrutura nodo.
+  */
+  Node(void);
+
+  //! Destrutor
+  /*!
+     Destrutor padrão da estrutura nodo.
+  */
+  ~Node(void);
+
+ private:
+  char letter;
+  Node* children[26];  // Pode ser uma 'Linked List' de ponteiros.
+  std::size_t position;
+  std::size_t length;
+};
+}  // namespace structures
+
+#endif  // INCLUDE_NODE_H_
