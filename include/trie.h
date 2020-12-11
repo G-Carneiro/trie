c3e7828a3f8a0c274fed225f0f58a0a584aec851
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-12-11 16:38:57 -0300

    WIP: número de prefixos

--- include/trie.h
+++ include/trie.h
@@ -1,72 +1,74 @@
 #ifndef STRUCTURES_TRIE_H
 #define STRUCTURES_TRIE_H
 
+#include <stdio.h>
 #include <string>
 
 #define ALPHABET_SIZE 26
 
 namespace structures {
-    
+
 struct TrieNode {
-	struct TrieNode* children[ALPHABET_SIZE];
-	int pos, len;
+  struct TrieNode* children[ALPHABET_SIZE];
+  int pos, len;
 };
 
 struct TrieNode* initNode() {
-	auto tNode = new TrieNode;
+  auto tNode = new TrieNode;
 
-	tNode->pos = 0;
-	tNode->len = 0;
+  tNode->pos = 0;
+  tNode->len = 0;
 
-	for (int i = 0; i < ALPHABET_SIZE; i++)
-		tNode->children[i] = NULL;
+  for (int i = 0; i < ALPHABET_SIZE; i++) tNode->children[i] = NULL;
 
-	return tNode;
+  return tNode;
 }
 
 void insert(struct TrieNode* root, std::string key, int pos, int len) {
-	auto it = root;
+  auto it = root;
 
-	for (int i = 0; i < key.length(); i++) {
-		int index = key[i] - 'a';
-		if (!it->children[index])
-			it->children[index] = initNode();
+  for (int i = 0; i < key.length(); i++) {
+    int index = key[i] - 'a';
+    if (!it->children[index]) it->children[index] = initNode();
 
-		it = it->children[index];
-	}
+    it = it->children[index];
+  }
 
-	it->pos = pos;
-	it->len = len;
+  it->pos = pos;
+  it->len = len;
 }
 
 std::pair<int, int> search(struct TrieNode* root, std::string key) {
-	std::pair<int, int> p;
-	auto it = root;
+  std::pair<int, int> p;
+  auto it = root;
 
-	for (int i = 0; i < key.length(); i++) {
-		int index = key[i] - 'a';
-		if (!it->children[index]) {
-			p.first = -1;
-			p.second = -1;
-			return p;
-		}
+  for (int i = 0; i < key.length(); i++) {
+    int index = key[i] - 'a';
+    if (!it->children[index]) {
+      p.first = -1;
+      p.second = -1;
+      return p;
+    }
 
-		it = it->children[index];
-	}
+    it = it->children[index];
+  }
 
-	if (it && it->len == 0) {
-		p.first = 0;
-		p.second = 0;
-		return p;
-	}
+  if (it && it->len == 0) {
+    p.first = 0;
+    p.second = 0;
+    return p;
+  }
 
-	p.first = it->pos;
-	p.second = it->len;
+  p.first = it->pos;
+  p.second = it->len;
 
-	return p;
+  if (p.first != -1) {
+	  // Find number of words in which string is prefix of
+  }
+
+  return p;
 }
 
 }  // namespace structures
 
 #endif
-
